c08c8ae3a29631746e44e29efa15b05fee00f7bc
Mohit Yadav <my1494810@gmail.com>
2025-12-26 18:16:38 +0530

    update

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import "./App.css";
+
+const App = () => {
+  const [task, setTask] = useState("");
+  const [taskList, setTaskList] = useState([]);
+
+  function handleSubmit() {
+    if(task.trim()===""){
+      return
+    }
+    setTaskList([...taskList, task]);
+    setTask("");
+  }
+
+  return (
+    <>
+      <h2>Todo</h2>
+
+      <input
+        type="text"
+        value={task}
+        placeholder="Enter your list "
+        onChange={(e) => {
+          setTask(e.target.value)
+        }}
+      />
+      <button onClick={handleSubmit}>Add</button>
+      <ul>
+        {taskList.map((num,index) => (
+          <li key={index} >{num}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default App;
---
+++ src/Components/form.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const form = () => {
+  return (
+   
+    <>
+        
+    </>
+  )
+}
+
+export default form
